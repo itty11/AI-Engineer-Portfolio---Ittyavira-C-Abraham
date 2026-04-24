b7e2affdd2a4d2a1c17d2a20aa2b5752d0cc61e1
itty11 <ittyavirac.abraham@gmail.com>
2026-04-24 10:04:46 +0530

    fixed url routing

--- backend/portfolio_project/urls.py
+++ backend/portfolio_project/urls.py
@@ -1,9 +1,7 @@
-# portfolio_project/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',   include('contact.urls')),
+    path('api/', include('contact.urls')),
 ]
