4d2661ef762cfc3a8372affb3730a09ae74fe7ef
itty11 <ittyavirac.abraham@gmail.com>
2026-07-11 09:20:27 +0530

    added LeetCode and HackerRank profiles

--- frontend/src/data/portfolioData.js
+++ frontend/src/data/portfolioData.js
@@ -8,6 +8,8 @@ export const personal = {
   email:     'ittyavirac.abraham@gmail.com',
   github:    'https://github.com/itty11',
   linkedin:  'https://www.linkedin.com/in/ittyavira-c-abraham-5435621b7/',
+  leetcode:  'https://leetcode.com/u/good_master11/',       
+  hackerrank:'https://www.hackerrank.com/profile/ittyavirac_abra1',
   resume:    'Ittyavira_C_Abraham_AI_Engineer_Resume.pdf',
   location:  'India',
   available:  true,
